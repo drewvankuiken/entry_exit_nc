cf3b6f23dabd8b68b63a07843cfd9d508655b44d
Drew Van Kuiken <83033747+drewvankuiken@users.noreply.github.com>
2022-11-11 11:57:28 -0500

    Add files via upload

---
+++ read_ipums_data.R
@@ -0,0 +1,81 @@
+# read_ipums_data
+# 2022.11.08
+# DVK
+
+################################################################################
+# file setup - for now, include this at the top of all scripts, eventually can 
+# split into separate maintenance file and only run once
+
+library(plyr)
+library(dplyr)
+
+# package installer - will install packages you haven't already installed
+need <- c('tidyverse','ipumsr','ggplot2','broom','lubridate','readxl','glue', 'tigris')
+have <- need %in% rownames(installed.packages())
+if(any(!have)) install.packages(need[!have])
+invisible(lapply(need,library,character.only=T))
+# ugly solution for the moment, but here() keeps going to wrong place
+#detach("package:here", unload=TRUE)
+
+# change path to root directory:
+### OVERALL DIRECTORY STRUCTURE: 
+# [folder for research project]
+## data, raw_data, scripts, tex
+### raw_data --> structure in zip file shared by Mengyang
+#### if you set things up as i did above, this should run on your machine with no changes
+script_folder = dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(glue('{script_folder}'))
+setwd('..')
+rm(list = ls())
+options(scipen = 999)
+library(here)
+
+# subfolders that we wanna use eventually
+rawdata <- here('raw_data')
+scripts <- here('scripts')
+data <- here('data')
+
+#-------------------------------------------------------------------------------
+# create dataset to merge onto
+# bc ACS reports fips for state and county separately, create 2 columns:
+# full fips (merge to CMS data), county fips (no leading zeros)
+fullfips <- seq(37001,37199,2)
+statefips <- seq(1,199,2)
+fipsdf_noyear <- data.frame(fullfips,statefips) %>% uncount(7)
+
+years <- 2014:2020
+yeardf <- data.frame(year= rep(years, times = 100))
+
+fipsdf <- data.frame(fipsdf_noyear, yeardf)
+
+#-------------------------------------------------------------------------------
+# read in ACS, merge to fipsdf
+ddi <- read_ipums_ddi(here(rawdata,"ACS/usa_00002.xml"))
+data <- read_ipums_micro(ddi)
+
+# puma-county crosswalk from 2010
+pumas_nc <- pumas(state = 37)
+counties_nc <- counties(state = 37, year = 2010)
+
+# TODO
+# coerce to same format, merge
+# find correct way to collapse
+# fit back to puf data 
+# FOR PUFS:
+# look at diff between projected claims and claims
+# see if entry predicted by size of gap
+
+puma_fip_xw <- read_delim(here(rawdata,"ACS/PUMSEQ10_37.txt"))
+
+
+# sample shell dataset with all counties hopefully
+counties <- data %>% select(YEAR, COUNTYFIP, HHINCOME, AGE) %>%
+  group_by(YEAR, COUNTYFIP) %>% 
+  dplyr::summarize(mean_inc = mean(HHINCOME), mean_age = mean(AGE))
+
+acs_shell <- fipsdf %>% 
+  left_join(counties, by = c("statefips" = "COUNTYFIP", "year"="YEAR"))
+
+save(acs_shell, file=here(data,"ACS.Rda"))
+
+
